69982d8c9cd3d64222b282a2e3464a149dcebaa9
BigGillie <biggillieaws@gmail.com>
2025-03-11 17:07:31 -0500

    Update config.js
    more of the same

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_pEiOo2gYk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'kdvuarmnnn2c0njag21uqfgop', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_DKaVZT5Ur', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3o0dakclt2ckh1vto7najau71h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
